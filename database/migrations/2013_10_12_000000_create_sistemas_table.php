66a04d521ebac919832b1388ce333599b4cb77ea
LORDjoaquin <L19270582@tuxtla.tecnm.mx>
2022-12-07 16:12:10 -0600

    tablar en la base de datos

---
+++ database/migrations/2013_10_12_000000_create_sistemas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSistemasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sistemas', function (Blueprint $table) {
+            $table->id();
+            $table->string('sistema');
+            $table->unsignedBigInteger('status_id');
+            $table->foreign('status_id')->references('id')->on('statuses');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sistemas');
+    }
+}
