55de1ab6472184e84aa29212a5143774fde470d1
LORDjoaquin <L19270582@tuxtla.tecnm.mx>
2022-12-14 16:04:19 -0600

    cambios y pruebas

---
+++ resources/views/buttons.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Buttons</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <h1>Buttons</h1>
+    <button type="button" class="btn btn-primary">Primary Button</button>
+    <button type="button" class="btn btn-secondary">Secondary Button</button>
+    <button type="button" class="btn btn-success">Success Button</button>
+</body>
+</html>
